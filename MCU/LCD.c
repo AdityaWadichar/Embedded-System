2dfe06903eba7ff5db1f9d6e6fc0c6ebe7962db7
Aditya Wadichar <adityawadichar85@gmail.com>
2020-10-23 18:53:26 +0530

    Update LCD.c

--- MCU/LCD.c
+++ MCU/LCD.c
@@ -12,19 +12,20 @@ void main()
 {
 	int i;
 	unsigned char *string = "Aditya";
-	int l = sizeof(string)/sizeof(string[0]);
-	P1_3 = 0;
+	int l = sizeof(string);
+	P2_3=0;
+	P0_3 = 0;
 	P1 = 0x38;
 	P0_2 = 1;
-	delay(50);
+	delay(250);
 	P0_2 = 0;
 	P1 = 0b00000110;
 	P0_2 = 1;
-	delay(50);
+	delay(250);
 	P0_2 = 0;
 	P1 = 0b00001100;
 	P0_2 = 1;
-	delay(50);
+	delay(250);
 	P0_2 = 0;
 
 	P0_3 = 1;
@@ -32,11 +33,10 @@ void main()
 	
 	for (i=0; i<=l; i++)
 	{
-		P2 = string[i];
+		P1 = string[i];
 		P0_2 = 1;
-		delay(50);
+		delay(250);
 		P0_2 = 0;
 	}
 }
-
 
