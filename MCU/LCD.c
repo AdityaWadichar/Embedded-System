631bbe89bcee0f595bb483cb9869961195594be9
Aditya Wadichar <adityawadichar85@gmail.com>
2020-10-24 22:16:36 +0530

    final code in keil

--- MCU/LCD.c
+++ MCU/LCD.c
@@ -1,42 +1,60 @@
 #include<reg51.h>
+#include<string.h>
 
-void delay(int t)
-{
-	while(t>0)
-	{
-		t--;
-	}
-}
+void LCD_set(unsigned char set);
+void LCD_input(unsigned char *input);
+void delay();
+
+
+sbit rs=P3^1;  // Register select pin
+sbit rw=P3^2;  // read/write pin
+sbit en=P3^3;  // enable pin
 
 void main()
 {
-	int i;
-	unsigned char *string = "Aditya";
-	int l = sizeof(string);
-	P2_3=0;
-	P0_3 = 0;
-	P1 = 0x38;
-	P0_2 = 1;
-	delay(250);
-	P0_2 = 0;
-	P1 = 0b00000110;
-	P0_2 = 1;
-	delay(250);
-	P0_2 = 0;
-	P1 = 0b00001100;
-	P0_2 = 1;
-	delay(250);
-	P0_2 = 0;
-
-	P0_3 = 1;
-
+	LCD_set(0x38);  // Setting LCD 2 lines, 5*7 matrix
+	LCD_set(0x0E);  // cursor & display on
+	LCD_set(0x06);  // increament cursor position
 	
-	for (i=0; i<=l; i++)
-	{
-		P1 = string[i];
-		P0_2 = 1;
-		delay(250);
-		P0_2 = 0;
-	}
+	LCD_input("Aditya Wadichar");
+	LCD_set(0xC0);
+	delay();
+	
 }
 
+void LCD_set(unsigned char set)
+{
+	rs = 0;  // command mode
+	rw = 0;  // write mode select
+	P2 = set;  // command
+	en = 1;
+	delay();
+	en = 0;
+	return;
+}
+
+void LCD_input(unsigned char *input)
+{
+	int i;
+	int l = strlen(input);
+	for (i=0; i<l; i++)   // string to be displayed
+	{
+		rw = 0;  // write mode
+		rs = 1;  // Data mode
+		P2 = input[i];  // sending character one by one
+		en = 1;
+		delay();
+		en = 0;
+	}
+	return;
+}
+
+void delay()
+{
+	int i = 1000;
+	while (i>=0)
+	{
+		i--;
+	}
+	return;
+}
